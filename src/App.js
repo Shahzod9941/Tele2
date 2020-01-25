ef89caa51dc419dbfd6008bc732620a39cd850a5
Shahzod9941 <shahzodtoato@gmail.com>
2020-01-25 15:31:26 +0500

    Tele2 initial version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Plan from './components/plan/Plan'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    const array = [
+        {name: 'Мой онлайн+', price: 700, limit:"безлимит на Tele2 в России", min: 800, gb: 30 , button: 'Купит SIM' },
+        {name: 'Везде онлайн', price: 500, limit:"безлимит на Tele2 в России", min: 500, gb: 40, button: 'Купит SIM'},
+        {name: 'Мой онлайн', price: 400, limit:"безлимит на Tele2 в России", min: 500, gb: 15, button: 'Купит SIM'},
+    ];
+    return(
+        <div>
+        <Plan name={array[0].name} price={array[0].price} limit={array[0].limit} min={array[0].min} gb={array[0].gb} button={array[0].button} />
+        <Plan name={array[1].name} price={array[1].price} limit={array[1].limit} min={array[1].min} gb={array[1].gb} button={array[1].button} />
+        <Plan name={array[2].name} price={array[2].price} limit={array[2].limit} min={array[2].min} gb={array[2].gb} button={array[2].button} />
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/plan/Plan.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Style.css'
+
+// export default function Plan(props){
+//     //console.log(props);
+//     return(
+//         <div class="block">
+//             <h3>{props.name}</h3>
+//             <h1>{props.price}</h1>
+//             <p>{props.limit}</p>
+//             <p>{props.min}</p>
+//             <p>{props.gb}</p>
+//             <center><button>{props.button}</button></center>
+//         </div>
+//     )
+// }
+
+export default function Plan(props) {
+    return (
+        <div className="basic-div">
+            <div className="tariff-h1">{props.name}</div>
+            <div className="box-summa">
+                <div className="price">{props.price}</div>
+                <div className="price-box">
+                    <div className="rubl">₽</div>
+                    <div className="price-mount">/месяц</div>
+                </div>
+            </div>
+            <div className="unlimited"><strong>безлимит</strong> на Теле2 России</div>
+            <div className="russia"><strong>{props.min}</strong> мин на остальные номера Росии</div>
+            <div className="gb"><strong>{props.gb} GB </strong> интернета</div><br />
+             <center><a href="https://msk.tele2.ru/"><div className="button">{props.button}</div></a></center>
+        </div>
+    )
+}
